d379edadf8ab8c05ddacaf0289dacd43fc60d037
Music Mad <chrisklepper1776@gmail.com>
2023-09-29 22:03:45 -0400

    Updated test.py to print average hsv val

--- test.py
+++ test.py
@@ -7,12 +7,14 @@ red = (467, 19, 135, 141)
 def red_pipeline(img:cv.Mat):
     imCrop = img[int(red[1]):int(red[1]+red[3]), int(red[0]):int(red[0]+red[2])]
     hsv = cv.cvtColor(imCrop, cv.COLOR_BGR2HSV)
-    cubePixels = 0
+    """ cubePixels = 0
     for row in imCrop:
         for h, _, _ in row:
             if h > 5 and h < 7:
                 cubePixels += 1
-    print(cubePixels)
+    print(cubePixels) """
+
+    print(cv.mean(hsv))
     cv.imshow("test", imCrop)
 
 
@@ -27,4 +29,6 @@ if __name__ == "__main__":
         ret, img = cam.read()
         red_pipeline(img)
         blue_pipeline(img)
-        cv.waitKey(0)
+        key = cv.waitKey(0)
+        if key == ord('q'):
+            break
