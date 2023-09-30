d7940d3e6fbccc537dc364e76d12bf2acd098099
Philip Smith <philip@randomsmiths.com>
2023-09-29 20:44:16 -0400

    points appear and disappeard

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask
+from flask import render_template
+from time import perf_time
+
 
 app = Flask(__name__)
 
-POINTS = 0
-TIME = 0
+start_time = 0
 
 @app.route("/")
-def main_screen():
+def main_screen(points=None):
+    if points:
+        return render_template('mainscreen.jinja', points=points)
+    return render_template('mainscreen.jinja')
+
+@app.route("/match")
+def during_match():
+    global start_time
+    pass
+
+def post_match():
+    global start_time
     pass
 
 @app.route("/start")
 def start():
-
-    return main_screen()
+    global start_time
+    start_time = perf_counter()
+    return during_match()
