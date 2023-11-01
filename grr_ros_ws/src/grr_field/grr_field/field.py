3cbae38ed91bc47fa7500bf65ebb5823adff6403
Philip Smith <philip@randomsmiths.com>
2023-10-31 21:11:47 -0400

    field camera response

--- grr_ros_ws/src/grr_field/grr_field/field.py
+++ grr_ros_ws/src/grr_field/grr_field/field.py
@@ -7,7 +7,14 @@ from rclpy.node import Node
 from rclpy.context import Context
 from rclpy.parameter import Parameter
 
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Header
+from sensor_msgs.msg import Image
+
+from cv_bridge import CvBridge
+
+from grr_field_interfaces.srv import ImageRequest
+
+import cv2
 
 
 class Field(Node):
@@ -15,6 +22,13 @@ class Field(Node):
         super().__init__("grr_field")
         self.publisher = self.create_publisher(Bool, "end_button", 10)
         self.subscription = self.create_subscription(Bool, "start", self.start, 10)
+        self.cams = [cv2.VideoCapture(0), cv2.VideoCapture(1)]
+        self.bridge = CvBridge()
+    
+    def cam_callback(self, request, response):
+        ret, image = self.cams[request.cam].read()
+        response.frame = self.bridge.cv2_to_imgmsg(image)
+        return response
     
     def start(self, data:Bool) -> None:
         self.get_logger().info(f"Got a message on start saying {data}")
